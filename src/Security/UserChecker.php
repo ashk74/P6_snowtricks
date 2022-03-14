e02b8f4ba08e61d54b93002e024ff872825b43f6
Jonathan Secher <secher.jonathan@gmail.com>
2022-03-14 02:09:38 +0100

    Update UserChecker file

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -9,6 +9,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 
+/**
+ * Add additional checks during user authentication
+ */
 class UserChecker implements UserCheckerInterface
 {
     private $emailVerifier;
@@ -31,6 +34,7 @@ class UserChecker implements UserCheckerInterface
             return;
         }
 
+        // If user is not verified resend verification email
         if (!$user->isVerified()) {
             $this->resendVerifEmail($user);
             throw new CustomUserMessageAccountStatusException('Votre email n\'est pas vérifié. Veuillez cliquer sur le lien contenu dans l\'e-mail qui vous a été envoyé');
@@ -43,7 +47,7 @@ class UserChecker implements UserCheckerInterface
             'app_verify_email',
             $user,
             (new TemplatedEmail())
-                ->from(new Address('secher.jonathan@gmail.com', 'SnowTricks - Confirmation email'))
+                ->from(new Address($_ENV['MAILER_EMAIL'], 'SnowTricks - Confirmation email'))
                 ->to($user->getEmail())
                 ->subject('Veuillez confirmer votre adresse email')
                 ->htmlTemplate('registration/confirmation_email.html.twig')
