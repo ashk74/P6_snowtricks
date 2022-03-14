cb52adcd34ae0f0f1850ba805b928f0143e80093
Jonathan Secher <secher.jonathan@gmail.com>
2022-03-14 02:16:40 +0100

    Create VideoController + VideoType form

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Video;
+use App\Repository\VideoRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,18 @@ class VideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('url')
-        ;
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Titre de la vidéo'
+                ]
+            ])->add('url', UrlType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'URL de la vidéo'
+                ],
+                'help' => "* Rendez vous sur Youtube ou Dailymotion, cliquer sur le bouton 'Partager' et copier le lien au format : https://youtu.be/videoID ou https://dai.ly/videoID"
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
