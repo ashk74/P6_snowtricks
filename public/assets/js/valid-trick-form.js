73aa2ae81e77f4ecd44dc8aacda14277f06d9498
Jonathan Secher <secher.jonathan@gmail.com>
2022-03-17 00:02:23 +0100

    Add Javascript scroller button + update views

--- public/assets/js/valid-trick-form.js
+++
@@ -1,7 +0,0 @@
-let trickName = document.getElementById("trick_name");
-let trickNameResult = document.getElementById("trick-name-result");
-trickName.oninput = countFieldChar(trickName, trickNameResult, 3);
-
-let trickContent = document.getElementById("trick_content");
-let trickContentResult = document.getElementById("trick-content-result");
-trickContent.oninput = countFieldChar(trickContent, trickContentResult, 50);
